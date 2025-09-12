efc72ac6702e5791a6d0db63c03ae4cbe04f5377
Vladislav Radovic <vradovic522@gmail.com>
2025-09-12 20:35:51 +0200

    Finish setup

--- TM/kjar/src/main/java/com/ftn/sbnz/kjar/KjarApplication.java
+++ TM/kjar/src/main/java/com/ftn/sbnz/kjar/KjarApplication.java
@@ -1,13 +1,9 @@
 package com.ftn.sbnz.kjar;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 public class KjarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KjarApplication.class, args);
+		System.out.println("Hello from kjar!");
 	}
 
 }
--- TM/model/src/main/java/com/ftn/sbnz/model/ModelApplication.java
+++ TM/model/src/main/java/com/ftn/sbnz/model/ModelApplication.java
@@ -1,13 +1,9 @@
 package com.ftn.sbnz.model;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 public class ModelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModelApplication.class, args);
+		System.out.println("Hello from model!");
 	}
 
 }
