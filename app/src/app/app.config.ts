0079bd29323e6beb027c25377d022186cc2ef613
Vladislav Radovic <vradovic522@gmail.com>
2025-10-04 10:09:50 +0200

    Initial frontend setup

---
+++ app/src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+      ripple: true,
+    }),
+  ],
+};
